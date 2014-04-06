dbebbd8bc18f719aac6474e7e69499419e7c2bec
Bernhard Schuster <schuster.bernhard@gmail.com>
2014-04-06 19:44:52 +0200

    license: Add LICENSE file and preamble to *.[ch] files.

--- src/goat-plot.h
+++ src/goat-plot.h
@@ -1,3 +1,23 @@
+/*
+ * goat-plot.h
+ * This file is part of GoatPlot
+ *
+ * Copyright (C) 2014 - Bernhard Schuster <schuster.bernhard@gmail.com>
+ *
+ * GoatPlot is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * GoatPlot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GoatPlot. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __GOAT_PLOT_H__
 #define __GOAT_PLOT_H__
 
